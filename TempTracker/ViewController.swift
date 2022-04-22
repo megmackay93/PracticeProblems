ab0adfc95c4e354150ad0225728c87969688d4d7
Megan Mackay <megmackay18@gmail.com>
2022-04-22 11:13:08 -0400

    Initial Commit

---
+++ TempTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempTracker
+//
+//  Created by Megan Mackay on 4/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
